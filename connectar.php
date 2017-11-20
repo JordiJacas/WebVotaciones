12d1e351f9f8793e9265ce1316cef4fba0148d1b
JordiJacas <jjacasventura@iesesteveterradas.cat>
2017-11-20 20:25:38 +0100

    ooO

--- connectar.php
+++ connectar.php
@@ -1,38 +1,10 @@
 <?php
-
-		$usr = $_POST['user'];
-		$psswd = $_POST['password'];
-		//connexió dins block try-catch:
-		//  prova d'executar el contingut del try
-		//  si falla executa el catch
-		try {
-		  $hostname = "localhost";
-		  $dbname = "Encuestas";
-		  $username = "Encuesta";
-		  $pw = "encuesta";
-		  $pdo = new PDO ("mysql:host=$hostname;dbname=$dbname","$username","$pw");
-		} catch (PDOException $e) {
-		  echo "Failed to get DB handle: " . $e->getMessage() . "\n";
-		  exit;
-		}
-
-		//preparem i executem la consulta
-		$query = $pdo->prepare("select * FROM Usuarios WHERE nombre = '".$usr."' and password = '".$psswd."'");
-		$query->execute();
-
-		//anem agafant les fileres d'amb una amb una
-		$row = $query->fetch();
-		if($row != null){
-			while ( $row ) {
-			  echo $row['id_user'] . "-". $row['nombre'] ."-". $row['apellido'] ."-". $row['email'] ."-". $row['password'] ."-". $row['isAdmin'];
-			  $row = $query->fetch();
-			}
-		}else{
-			header('Location: http://localhost/WebVotaciones/');
-		}
-
-		//eliminem els objectes per alliberar memòria 
-		unset($pdo); 
-		unset($query)
-
+	function getDatos($idUser, $nombreUser, $apellidoUser, $emailUser, $psswd, $isAdmin){
+		echo $iduser;
+		echo $nombreUser;
+		echo $apellidoUser;
+		echo $emailUser;
+		echo $psswd;
+		echo $isAdmin;
+	}
 ?>
--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 </head>
 <body>
 	<div id = "inicioSession">
-		<form action = "connectar.php" method = "post">
+		<form action = " " method = "post">
 			<label>Name: </label> <br>
 			<input type="text" name="user"/> <br> <br>
 			<label>Password: </label> <br>
@@ -15,14 +15,56 @@
 			<input class = "submit" type="submit" name="submitIniciar" value="Enviar"/>
 		</form>
 	</div>
+	<?php
+		session_start();
+		include 'connectar.php';
 
-	<div id = "consulta">
-		<form action = "" method="post">
-			<label>Escribe aque la consulta: </label> <br>
-			<input type="textArea" name="consulta">
-			<input class = "submit" type="submit" name="submitConsulta" value = "Enviar">
-		</form>
-	</div>
+		if(!isset($_SESSION['usr']) || !isset($_SESSION['password'])){
+			$_SESSION['usr'] = " ";
+			$_SESSION['password'] = " ";
+			
+		}else{
+			$_SESSION['usr'] = $_POST['user'];
+			$_SESSION['password'] = $_POST['password'];
+		}		
+			//connexió dins block try-catch:
+			//  prova d'executar el contingut del try
+			//  si falla executa el catch
+			try {
+			  $hostname = "localhost";
+			  $dbname = "Encuestas";
+			  $username = "Encuesta";
+			  $pw = "encuesta";
+			  $pdo = new PDO ("mysql:host=$hostname;dbname=$dbname","$username","$pw");
+			} catch (PDOException $e) {
+			  echo "Failed to get DB handle: " . $e->getMessage() . "\n";
+			  exit;
+			}
 
+			//preparem i executem la consulta
+			$query = $pdo->prepare("select * FROM Usuarios WHERE nombre = '".$_SESSION['usr']."' and password = '".$_SESSION['password']."'");
+			$query->execute();
+
+			//anem agafant les fileres d'amb una amb una
+			$row = $query->fetch();
+			if($row != null){
+				while ( $row ) {
+				  $idUser = $row['id_user'];
+				  $nombreUser = $row['nombre'];
+				  $apellidoUser  = $row['apellido'];
+				  $emailUser = $row['email'];
+				  $psswd = $row['password'];
+				  $isAdmin = $row['isAdmin'];
+				  getDatos($idUser, $nombreUser, $apellidoUser, $emailUser, $psswd, $isAdmin);
+				  $row = $query->fetch();
+				}
+				header('Location: http://localhost/WebVotaciones/connectar.php');
+			}
+
+			//eliminem els objectes per alliberar memòria 
+			unset($pdo); 
+			unset($query);	
+
+?>
 </body>
 </html>
