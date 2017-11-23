5f03045dd34f4619bc7da8c66c17bff366aed07b
JordiJacas <32141374+JordiJacas@users.noreply.github.com>
2017-11-23 22:22:38 +0100

    enviar

--- php/enviarConsulta.php
+++ php/enviarConsulta.php
@@ -1,4 +1,5 @@
 <?php
+	session_start();
 	include "funcions.php";
 	
 	$pdo = connectar();
@@ -6,12 +7,23 @@
 	$consulta = $_POST['consulta'];
 	$array = $_POST['i'];
 	$row = $_SESSION['row'];
-	
-	print_r($array);
-	echo $consulta;
-	echo $row['id_user']
 
 	//preparem i executem la consulta
-	//$query = $pdo->prepare("insert into Consultas (descripcion, id_admin) values ('".$consula."',".$_SESSION['id_user']."")");
-	//$query->execute();
+	$query = $pdo->prepare("insert into consultas (descripcion, id_admin) values ('".$consulta."',".$row['id_user'].")");
+	$query->execute();
+	
+	$query = $pdo->prepare("select id_consulta from consultas where descripcion = '".$consulta."'");
+	$query->execute();
+	$dades = $query->fetch();
+	
+	foreach($array as $opcio){
+		$query = $pdo->prepare("insert into opciones (id_consulta, texto) values (".$dades['id_consulta'].",'".$opcio."')");
+		$query->execute();
+	}
+	
+	//eliminem els objectes per alliberar memòria 
+	unset($pdo); 
+	unset($query);
+	
+
 ?>
