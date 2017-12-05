4b423b28772adcb2ddd55b8d7b9e69a881cd38b3
JordiJacas <jjacasventura@iesesteveterradas.cat>
2017-12-05 17:29:45 +0100

    votar

--- php/funcions.php
+++ php/funcions.php
@@ -71,11 +71,11 @@
 		while($opciones){
 			
 			//decidim si el input(radio) esta checked o no.
-			//if($opciones['id_opcion'] == $voto['id_opcion']){
-			//	echo "<input type='radio' name='respuesta' value='".$opciones['id_opcion']."' checked>".$opciones['texto']."";
-			//}else if($opciones['id_opcion'] != $voto['id_opcion']){
+			if($opciones['id_opcion'] == $voto['id_opcion']){
+				echo "<input type='radio' name='respuesta' value='".$opciones['id_opcion']."' checked>".$opciones['texto']."";
+			}else if($opciones['id_opcion'] != $voto['id_opcion']){
 				echo "<input type='radio' name='respuesta' value='".$opciones['id_opcion']."'>".$opciones['texto']."";
-			//}
+			}
 		
 			$opciones = $query->fetch();
 		}
