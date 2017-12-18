d64764b873eb3ffdc096a67f6cce5f51a65a79e2
JordiJacas <jjacasventura@iesesteveterradas.cat>
2017-12-18 19:03:14 +0100

    ema

--- php/email.php
+++ php/email.php
@@ -5,7 +5,6 @@
 	$email = $_POST['email'];
 	$id_consulta = $_POST['idConsulta'];
 
-	echo $id_consulta;
 
 	$email = explode("\n", $email);
 
@@ -20,22 +19,26 @@
 		if($numUser = 0){
 			$query = $pdo->prepare("INSERT into Usuarios (email,isAdmin,token) values ('jjacas@es.com',0,'abcd234')");
 			$query->execute();
+
 		}else if ($numUser = 1){
 			$query = $pdo->prepare("UPDATE Usuarios set token = '".$token."' WHERE email = '".$email[$num]."'");
 			$query->execute();
+
 		}
+
 
 		$query = $pdo->prepare("SELECT * FROM Usuarios WHERE token = '".$token."' AND email = '".$email[$num]."'");
 		$query->execute();
 		$usuario = $query->fetch();
 
+
 		$query = $pdo->prepare("INSERT into Invitaciones (id_admin,id_consulta) values (".$usuario['id_user'].",".$id_consulta.")");
 		$query->execute();
-		
 		$usuario = $query->fetch();
+
 		
-		$mensaje = "http://localhost/WebVotaciones/?token=".$token;
-		mail($email[num], $titulo, $mensaje);
+		$mensaje = "http://jjacas.tk/~app/WebVotaciones/?token=".$token;
+		mail($email[$num], $titulo, $mensaje);
 	}
 
 	header('Location: http://jjacas.tk/~app/WebVotaciones/php/menuPrincipal.php');
